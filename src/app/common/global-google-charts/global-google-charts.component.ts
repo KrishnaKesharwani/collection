30fcce305aa488d39af4ccc6d283e47bb2aad17e
KratiBakshi <114287776+KratiBakshi@users.noreply.github.com>
2024-10-16 13:38:42 +0530

    Google Chart Global Level

--- src/app/common/global-google-charts/global-google-charts.component.ts
+++ src/app/common/global-google-charts/global-google-charts.component.ts
@@ -27,11 +27,39 @@ export class GlobalGoogleChartsComponent {
     var options = {
       'title': 'How Much Pizza I Ate Last Night',
       'width': 400,
-      'height': 300
+      'height': 300,
+      pieHole: 0.4,
     };
-    const element = document.getElementById('chart_div') as HTMLElement
-    var chart = new google.visualization.PieChart(element);
-    chart.draw(data, options);
+
+    // code for donut chart
+    // var data2 = google.visualization.arrayToDataTable([
+    //   ['Task', 'Hours per Day'],
+    //   ['Work', 11],
+    //   ['Eat', 2],
+    //   ['Commute', 2],
+    //   ['Watch TV', 2],
+    //   ['Sleep', 7]
+    // ]);
+
+    // var options2 = {
+    //   title: 'My Daily Activities',
+    //   pieHole: 0.4,
+    // };
+
+    const columnElement = document.getElementById('column_div') as HTMLElement
+    const pieElement = document.getElementById('pie_div') as HTMLElement
+    const comboElement = document.getElementById('combo_div') as HTMLElement
+    const donutElement = document.getElementById('donut_div') as HTMLElement
+
+    var columnChart = new google.visualization.ColumnChart(columnElement);
+    var pieChart = new google.visualization.PieChart(pieElement);
+    var comboChart = new google.visualization.ComboChart(comboElement);
+    var donutChart = new google.visualization.PieChart(donutElement);
+
+    columnChart.draw(data, options);
+    pieChart.draw(data, options);
+    comboChart.draw(data, options);
+    donutChart.draw(data, options);
   }
 
 }
