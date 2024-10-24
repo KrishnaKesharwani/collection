5c9b5172d4ef0400ad6f8c5c098b95098a178770
KrishnaKesharwani <31506398+KrishnaKesharwani@users.noreply.github.com>
2024-10-24 23:12:28 +0530

    Google Chart Added in dashboard

--- src/app/common/global-google-charts/global-google-charts.component.ts
+++ src/app/common/global-google-charts/global-google-charts.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { height } from '@mui/system';
 import { GoogleChartsModule } from 'angular-google-charts';
 
 @Component({
@@ -29,13 +30,13 @@ export class GlobalGoogleChartsComponent {
   };
   ngOnInit() {
     google.charts.load('current', { packages: ['corechart'] });
-    google.charts.setOnLoadCallback(this.drawChart);
+    // google.charts.setOnLoadCallback(this.drawChart);
     if (this.currentuserType == 0) {
-      this.drawMastercharts();
+      google.charts.setOnLoadCallback(this.drawMastercharts);
     } else if (this.currentuserType == 1) {
-      this.drawCompanycharts();
+      google.charts.setOnLoadCallback(this.drawCompanycharts);
     } else if (this.currentuserType == 2) {
-      this.drawMembercharts();
+      google.charts.setOnLoadCallback(this.drawMembercharts);
     } else if (this.currentuserType == 3) {
       // this.drawUsercharts();
       google.charts.setOnLoadCallback(this.drawUsercharts);
@@ -46,11 +47,73 @@ export class GlobalGoogleChartsComponent {
 
   }
   drawCompanycharts() {
-
+    var userdataColoum = google.visualization.arrayToDataTable([
+      ['Days', 'Amount'],
+      ['Nov', 15000],
+      ['Oct', 20000],
+      ['Sep', 13000],
+      ['Aug', 13000],
+      ['July', 22000],
+    ]);
+    var comboOptions = {
+      vAxis: { title: 'Amount' },
+      hAxis: { title: 'Days' },
+      is3D: true,
+      tooltip: {trigger: 'selection'},
+    };
+    var assignreceived = google.visualization.arrayToDataTable([
+      ['Amount', 'Count'],
+      ['Total Amount', 500000],
+      ['Pending Amount', 200000],
+    ]);
+    var options = {
+      is3D: true,
+      pieHole: 0.4,
+    };
+    const columnUserElement = document.getElementById('column_company_chart') as HTMLElement;
+    const pieUserElement = document.getElementById('pie_company_chart') as HTMLElement;    
+    var columnUserChart = new google.visualization.ColumnChart(columnUserElement);
+    columnUserChart.draw(userdataColoum, comboOptions);
+    var pieUserChart = new google.visualization.PieChart(pieUserElement);
+    pieUserChart.draw(assignreceived, options);
   }
   drawMembercharts() {
-
+    var userdataColoum = google.visualization.arrayToDataTable([
+      ['Days', 'Amount'],
+      ['24 Nov', 15000],
+      ['23 Nov', 20000],
+      ['22 Nov', 13000],
+      ['21 Nov', 22000],
+      ['20 Nov', 14000],
+      ['19 Nov', 14000],
+      ['18 Nov', 13000],
+      ['17 Nov', 22000],
+      ['16 Nov', 14000],
+      ['15 Nov', 14000],
+    ]);
+    var comboOptions = {
+      vAxis: { title: 'Amount' },
+      hAxis: { title: 'Days' },
+      is3D: true,
+      tooltip: {trigger: 'selection'},
+    };
+    var assignreceived = google.visualization.arrayToDataTable([
+      ['Assign', 'Received'],
+      ['Pending Assign', 10],
+      ['Received Assign', 5],
+    ]);
+    var options = {
+      is3D: true,
+      pieHole: 0.4,
+    };
+    const columnUserElement = document.getElementById('column_member_chart') as HTMLElement;
+    const pieUserElement = document.getElementById('pie_member_chart') as HTMLElement;    
+    var columnUserChart = new google.visualization.ColumnChart(columnUserElement);
+    columnUserChart.draw(userdataColoum, comboOptions);
+    var pieUserChart = new google.visualization.PieChart(pieUserElement);
+    pieUserChart.draw(assignreceived, options);
   }
+
   drawUsercharts() {
     var userdataColoum = google.visualization.arrayToDataTable([
       ['Month', 'Deposit', 'Creadit'],
@@ -85,53 +148,53 @@ export class GlobalGoogleChartsComponent {
     pieUserChart.draw(loandata, options);
   }
 
-  drawChart() {
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Topping');
-    data.addColumn('number', 'Slices');
-    data.addRows([
-      ['Mushrooms', 3],
-      ['Onions', 1],
-      ['Olives', 1],
-      ['Zucchini', 1],
-      ['Pepperoni', 2]
-    ]);
-    var options = {
-      'title': 'How Much Pizza I Ate Last Night',
-      'width': 400,
-      'height': 300,
-      is3D: true,
-    };
+  // drawChart() {
+  //   var data = new google.visualization.DataTable();
+  //   data.addColumn('string', 'Topping');
+  //   data.addColumn('number', 'Slices');
+  //   data.addRows([
+  //     ['Mushrooms', 3],
+  //     ['Onions', 1],
+  //     ['Olives', 1],
+  //     ['Zucchini', 1],
+  //     ['Pepperoni', 2]
+  //   ]);
+  //   var options = {
+  //     'title': 'How Much Pizza I Ate Last Night',
+  //     'width': 400,
+  //     'height': 300,
+  //     is3D: true,
+  //   };
 
-    // code for donut chart
-    // var data2 = google.visualization.arrayToDataTable([
-    //   ['Task', 'Hours per Day'],
-    //   ['Work', 11],
-    //   ['Eat', 2],
-    //   ['Commute', 2],
-    //   ['Watch TV', 2],
-    //   ['Sleep', 7]
-    // ]);
+  //   // code for donut chart
+  //   // var data2 = google.visualization.arrayToDataTable([
+  //   //   ['Task', 'Hours per Day'],
+  //   //   ['Work', 11],
+  //   //   ['Eat', 2],
+  //   //   ['Commute', 2],
+  //   //   ['Watch TV', 2],
+  //   //   ['Sleep', 7]
+  //   // ]);
 
-    // var options2 = {
-    //   title: 'My Daily Activities',
-    //   pieHole: 0.4,
-    // };
+  //   // var options2 = {
+  //   //   title: 'My Daily Activities',
+  //   //   pieHole: 0.4,
+  //   // };
 
-    const columnElement = document.getElementById('column_div') as HTMLElement
-    const pieElement = document.getElementById('pie_div') as HTMLElement
-    const comboElement = document.getElementById('combo_div') as HTMLElement
-    const donutElement = document.getElementById('donut_div') as HTMLElement
+  //   const columnElement = document.getElementById('column_div') as HTMLElement
+  //   const pieElement = document.getElementById('pie_div') as HTMLElement
+  //   const comboElement = document.getElementById('combo_div') as HTMLElement
+  //   const donutElement = document.getElementById('donut_div') as HTMLElement
 
-    var columnChart = new google.visualization.ColumnChart(columnElement);
-    var pieChart = new google.visualization.PieChart(pieElement);
-    var comboChart = new google.visualization.ComboChart(comboElement);
-    var donutChart = new google.visualization.PieChart(donutElement);
+  //   var columnChart = new google.visualization.ColumnChart(columnElement);
+  //   var pieChart = new google.visualization.PieChart(pieElement);
+  //   var comboChart = new google.visualization.ComboChart(comboElement);
+  //   var donutChart = new google.visualization.PieChart(donutElement);
 
-    columnChart.draw(data, options);
-    pieChart.draw(data, options);
-    comboChart.draw(data, options);
-    donutChart.draw(data, options);
-  }
+  //   columnChart.draw(data, options);
+  //   pieChart.draw(data, options);
+  //   comboChart.draw(data, options);
+  //   donutChart.draw(data, options);
+  // }
 
 }
