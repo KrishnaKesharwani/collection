e17d921132dbade9a6aee05bf13c317559faf75f
KratiBakshi <114287776+KratiBakshi@users.noreply.github.com>
2024-12-24 11:52:36 +0530

    Leave Member UI Design

--- src/app/components/member-list/leave-member/leave-member.component.ts
+++ src/app/components/member-list/leave-member/leave-member.component.ts
@@ -1,9 +1,12 @@
 import { ChangeDetectorRef, Component, Inject } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { CommonComponentService } from 'src/app/common/common-component.service';
+import { CustomerService } from 'src/app/services/customer/customer.service';
+import { DailyCollectionService } from 'src/app/services/dailyCollection/daily-collection.service';
+import { LoanService } from 'src/app/services/loan/loan.service';
 import { MemberService } from 'src/app/services/member/member.service';
 
 @Component({
@@ -14,17 +17,124 @@ import { MemberService } from 'src/app/services/member/member.service';
 export class LeaveMemberComponent {
   member_id: any;
   loading: any;
+  data: any;
+  selectControl = new FormControl('');
+  memberForm!: FormGroup;
+  // join_date: string|Date|null;
+  date: string = '';
+  member_login_id: string = '';
+  company_id: any;
+  assignDepositData: any;
+  loanList: any;
+  loanListData: any;
+  total_assignlength = 0;
 
-  constructor(public dialogRef: MatDialogRef<LeaveMemberComponent>, private cdr: ChangeDetectorRef, public _toastr: ToastrService, public router: Router, public _service: MemberService, public dropdownService: CommonComponentService, public fb: FormBuilder, public dialog: MatDialog, @Inject(MAT_DIALOG_DATA) public dataa: { title: string; subTitle: string, data: any }) { }
+  constructor(public dialogRef: MatDialogRef<LeaveMemberComponent>, private cdr: ChangeDetectorRef, public _toastr: ToastrService, public router: Router, public _service: MemberService, public dropdownService: CommonComponentService, public fb: FormBuilder, public dialog: MatDialog, @Inject(MAT_DIALOG_DATA) public dataa: { title: string; subTitle: string, data: any }, public _loanService: LoanService, public _dailyService: DailyCollectionService,
+    public _customerService: CustomerService) { }
   ngOnInit() {
 
+    const data = localStorage.getItem('CurrentUser');
+    if (data) {
+      const userData = JSON.parse(data);
+      this.company_id = userData.company_id;
+    }
+    this.memberForm = this.fb.group({
+
+      date: [new Date(), Validators.required],
+      assigned_member_id: ['']
+    });
+    this.company_id = this.company_id
+    this.member_id = this.dataa.data.id;
+
+    this.viewMemberDetails();
+    this.getAssignDeposit();
+    this.getAssignLoan();
+    this.getAssignCount();
   }
 
   onClose() {
     this.dialogRef.close();
   }
 
-  submit() {
+  viewMemberDetails() {
+    this.data = this.dataa.data;
+    this.getActiveMemberList();
 
+  }
+
+  submit() {
+    this.loading = true;
+    if (this.memberForm.valid) {
+
+    } else {
+      this.memberForm.markAllAsTouched();
+    }
+  }
+
+  memberdata: [] = [];
+  getActiveMemberList() {
+    let obj = {
+      company_id: this.company_id,
+      status: 'active'
+    }
+    this._customerService.activeMembers(obj).subscribe((memberData: any) => {
+      this.memberdata = memberData.data;
+      const members = memberData.data.map((member: any) => member.name);
+      this.dropdownService.setOptions('assingmember', memberData.data);
+    })
+  }
+
+  getAssignDeposit() {
+    this.loading = true;
+    let obj = {
+      company_id: this.company_id,
+      status: 'active',
+      member_id: this.member_id,
+    }
+    this._dailyService.getDepositListForCustomer(obj).subscribe((data: any) => {
+      this.assignDepositData = data.data.deposits;
+      this.loading = false;
+    }, error => {
+      this.loading = false;
+    })
+  }
+
+
+  getAssignLoan() {
+    this.loading = true;
+    let obj = {
+      company_id: this.company_id,
+      loan_status: 'paid',
+      status: 'active',
+      member_id: this.member_id
+    }
+    this._loanService.getMemberLoanList(obj).subscribe((data: any) => {
+      if (data.success) {
+        this.loanList = data.data.loans;
+        this.loanListData = data.data;
+        this.loading = false;
+        console.log('Availble Loan List', this.loanList);
+      } else {
+        this.loanList = 0;
+        // this._toaster.error(data.message, 'Error')
+      }
+    }, error => {
+      this.loading = false;
+      console.log('Availble Loan List', this.loanList);
+    });
+  }
+
+  getAssignCount() {
+    setTimeout(() => {
+      if (this.loanList?.length) {
+        this.total_assignlength = this.total_assignlength + this.loanList?.length;
+      }
+      if (this.assignDepositData?.length) {
+        this.total_assignlength = this.total_assignlength + this.assignDepositData?.length;
+
+      }
+      // this.total_assignlength = this.assignDepositData?.length + this.loanList?.length;
+      this.loading = false;
+    }, 1000);
   }
 }
--- src/app/components/member-list/view-member-list/view-member-list.component.ts
+++ src/app/components/member-list/view-member-list/view-member-list.component.ts
@@ -1,10 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 
-interface Transaction {
-  item: string;
-  cost: number;
-}
+
 
 @Component({
   selector: 'app-view-member-list',
@@ -16,18 +13,7 @@ interface Transaction {
 export class ViewMemberListComponent {
   member_details: any = [];
 
-  displayedColumns: string[] = ['item', 'cost'];
-  transactions: Transaction[] = [
-    { item: 'Member No.', cost: 4 },
-    { item: 'Profile Image', cost: 5 },
-    { item: 'Member Name', cost: 2 },
-    { item: 'Mobile', cost: 4 },
-    { item: 'Email Id', cost: 25 },
-    { item: 'Aadhar No.', cost: 15 },
-    { item: 'Join Date', cost: 15 },
-    { item: 'Status ', cost: 15 },
-    { item: 'Address ', cost: 15 },
-  ];
+
   data: any;
   constructor(public dialog: MatDialog, @Inject(MAT_DIALOG_DATA) public dataa: { title: string; subTitle: string, data: any },
   ) { }
